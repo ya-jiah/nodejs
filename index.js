f29c9126101e1b77393ea7d732afcc7a0b71f621
ya-jiah <anhuilue200210@126.com>
2021-02-24 18:00:18 +0800

    下一个路由

--- index.js
+++ index.js
@@ -1,39 +1,9 @@
-// var express = require('express');
-// var app = express();
-// app.get('/',function(request,response){
-//     response.send('Hello World');
-// })
-// var server = app.listen(8081,function(){
-//     var host = server.address().address;
-//     var prot = server.address().port;
-//     console.log('访问网址为：',host,prot)
-//  })
-
-var http = require('http');
-var querystring = require('querystring');
-var contents = querystring.stringify({//序列化一个对象
-        usr: 'zhang',
-        psw: '123456'
+var express = require('express');
+var app = express();
+    app.listen(8081,function(){
+        console.log('开始监听8081端口');
     });
-var option = {
-        method: 'post',
-        host: 'www',
-        prot: '80',
-        path:'/nodejs/index.php',
-        headers: {
-            "Content-Type":"application/x-www-form-urlencoded; charset=utf8",
-            "Content-Length":contents.length
-        }
-    };
-var Dispose = function(response){
-    var body='';
-    response.on('data',function(data){
-        body += data;
+    //get用来接收get请求， 第一个参数是检测符合的请求路径，第二个参数是回调函数
+    app.get('/',function(request,response){
+        response.send(request.protocol);
     });
-    response.on('end',function(){
-        console.log(body);
-    });
-};
-var req = http.request(option,Dispose);
-req.write(contents);//发送内容
-req.end();
