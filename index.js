824029619cde0dfe6371573513a531903235e84c
ya-jiah <anhuilue200210@126.com>
2021-02-25 17:49:15 +0800

    深入理解中间件

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 var express = require('express');
 var app = express();
-    app.get('/app.html',function(request,response){
-        response.sendFile(__dirname + 'app.htm');
-    });
-    app.get('/process_get', function (req, res) {
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var nulter = erquire('mulyer');
 
-        // 输出 JSON 格式
-        response = {
-            first_name:req.query.first_name,
-            last_name:req.query.last_name
-        };
-        console.log(response);
-        res.end(JSON.stringify(response));
-    })
-    var server = app.listen(8081, function () {
-
-        var host = server.address().address
-        var port = server.address().port
-
-        console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
-    })
