0768906a8f9bdba965b50d54e655b5e19bee8e27
ya-jiah <anhuilue200210@126.com>
2021-02-25 08:18:13 +0800

    中间件

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
 var express = require('express');
 var app = express();
-    app.listen(8081,function(){
-        console.log('开始监听8081端口');
+    app.get('/app.html',function(request,response){
+        response.sendFile(__dirname + 'app.htm');
     });
-    //get主页
-    app.get('/',function(request,response){
-        console.log('主页请求！');
-        response.send('Hello Get');
-    });
-    //post
-    app.post('/',function(request,response){
-        console.log('POST请求！');
-        response.send('Post主页');
-    });
-    //访问/del_user路径时触发删除
-    app.delete('/del_user',function(request,response){
-        console.log('请求del_user的delete！');
-        response.send('删除del_user');
-    });
-    //get请求list_user页面
-    app.get('/list_user',function(request,response){
-        console.log('请求list_user！');
-        response.send('get请求list_user');
-    });
-    //对页面 abcd, abxcd, ab123cd, 等响应 GET 请求
-    app.get('/ab*cd',function(request,response){
-        console.log('/ab*cd匹配get请求');
-        response.send('ab*cd页面');
-    });
+    app.get('/process_get', function (req, res) {
 
+        // 输出 JSON 格式
+        response = {
+            first_name:req.query.first_name,
+            last_name:req.query.last_name
+        };
+        console.log(response);
+        res.end(JSON.stringify(response));
+    })
+    var server = app.listen(8081, function () {
+
+        var host = server.address().address
+        var port = server.address().port
+
+        console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+    })
